5b92f6ebb6acf8b8d3a75105fb70835625abe83a
Fernando-Ligabue <76406175+Fernando-Ligabue@users.noreply.github.com>
2022-07-07 15:14:55 +0100

    RL SHORTENER

---
+++ src/config/Constants.ts
@@ -0,0 +1,4 @@
+export const config = { 
+    API_URL: 'http://localhost:5000',
+    MONGO_CONNECTION :'mongodb+srv://<user>:<password>@<database>.xrrxo.mongodb.net/url-shortener?retryWrites=true&w=majority'
+}
